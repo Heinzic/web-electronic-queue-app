0c4fb8b9d4eb489c832753ae86a3dd56580665be
Daniil Ryzhenko <ryzhenko62@gmail.com>
2025-03-18 17:26:51 +0500

    save on submit

--- src/components/pages/DateAndTime.tsx
+++ src/components/pages/DateAndTime.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux'
 import DatePicker from "../ui/datepicker"
 import { RadioCards, Text } from "@radix-ui/themes"
 import { Button } from "../ui/button";
 import { setSelectedDate, setSelectedTimeSlot } from '../../store/dateTimeSlice';
-import { RootState } from '../../store/store';
 
 interface TimeSlot {
   id: string;
@@ -12,8 +11,11 @@ interface TimeSlot {
 }
 
 export default function DateAndTime() {
+
+  const [date, setDate] = useState<Date | undefined>()
+  const [time, setTime] = useState<string | undefined>();
+
   const dispatch = useDispatch();
-  const { selectedDate, selectedTimeSlot } = useSelector((state: RootState) => state.dateTimeSlice);
 
   const timeSlots: TimeSlot[] = [
     { id: "1", time: "09:00 - 10:00" },
@@ -24,43 +26,41 @@ export default function DateAndTime() {
     { id: "6", time: "15:00 - 16:00" },
   ];
 
-  const handleDateChange: React.Dispatch<React.SetStateAction<Date | undefined>> = (dateOrFunction) => {
-    const newDate = typeof dateOrFunction === 'function' ? dateOrFunction(selectedDate ? new Date(selectedDate) : undefined) : dateOrFunction;
-    dispatch(setSelectedDate(newDate ? newDate.toISOString() : null));
+  const handleSubmit = () => {
+    dispatch(setSelectedDate(date ? date.toISOString() : null));
+    dispatch(setSelectedTimeSlot(time ? time : null));
   };
 
-  const handleTimeSlotChange = (value: string) => {
-    dispatch(setSelectedTimeSlot(value));
-  };
-
-  const isFormComplete = selectedDate && selectedTimeSlot;
+  const isFormComplete = date && time;
 
   return (
     <div className="flex flex-col justify-center">
       <div className="flex justify-center gap-[30px] flex-wrap">
           <div className="flex flex-col items-center">
             <DatePicker 
-              date={selectedDate ? new Date(selectedDate) : undefined} 
-              setDateFunc={handleDateChange} 
+              date={date ? new Date(date) : undefined} 
+              setDateFunc={setDate} 
               width={400} 
               height={200} 
               iconSize={80} 
               textSize={30}
             />
             <div className="mx-auto mt-[40px] flex gap-[15px]">
-              <Button variant="destructive">Отмена</Button>
-              <Button variant="default" disabled={!isFormComplete}>Записаться</Button>
+              <Button variant="destructive" size={'lg'} className='cursor-pointer'>Отмена</Button>
+              <Button variant="default" size={'lg'} disabled={!isFormComplete} 
+              onClick={handleSubmit}
+              className='cursor-pointer'>Записаться</Button>
             </div>
         </div>
-        {selectedDate && (
+        {date && (
           <RadioCards.Root 
             columns={{ sm: "3" }} 
             size={'3'} 
             color="crimson" 
             highContrast 
             className="max-w-[700px] w-[100%]"
-            onValueChange={handleTimeSlotChange}
-            value={selectedTimeSlot || undefined}
+            onValueChange={(selectedTime) => setTime(selectedTime)}
+            value={time || undefined}
           >
             {timeSlots.map((slot) => (
               <RadioCards.Item key={slot.id} value={slot.id}>
